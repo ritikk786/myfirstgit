8085174bf6e7d354887e23f084c2f1f0d9e8a1d5
ritikk786 <kumarritik9540@gmail.com>
2023-02-08 14:14:16 +0530

    task4

---
+++ task4ass2.js
@@ -0,0 +1,8 @@
+const items=document.getElementsByClassName('list-group-item')
+// console.log(items[0].textContent)
+items[2].style.backgroundColor='green';
+
+// make all the element bold
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold'
+}
