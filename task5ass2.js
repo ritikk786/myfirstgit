05555214b50eb0544ae835ecc5b80a818f4297c4
ritikk786 <kumarritik9540@gmail.com>
2023-02-08 15:11:25 +0530

    task5as2

---
+++ task5ass2.js
@@ -0,0 +1,9 @@
+let liclass=document.getElementsByClassName('list-group-item');
+for(let i=0;i<liclass.length;i++){
+    liclass[i].style.color='red'
+}
+
+let litag=document.getElementsByTagName('li')
+for(let i=0;i<litag.length;i++){
+    litag[i].style.backgroundColor='skyblue'
+}
