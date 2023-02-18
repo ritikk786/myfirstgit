49b0d3a4b45f7849203b92112b0040d07258701b
ritikk786 <kumarritik9540@gmail.com>
2023-02-18 16:27:21 +0530

    expensetracker

--- main.js
+++ main.js
@@ -1,109 +1,46 @@
-        var btn = document.getElementsByTagName('button')
-        var username=document.getElementById('name');
-        var userphone=document.getElementById('phone');
-        var useremail=document.getElementById('mail')
-        var msg=document.querySelector('.text-danger');
-        var itemul=document.querySelector('.items');
-        var form = document.getElementById('my-form')
-        
-        form.addEventListener('submit',onSubmit);
-
-        itemul.addEventListener('click',removeitem);
-        itemul.addEventListener('click',edititem);
-
-        
-        // create a function
-        function onSubmit(e){
-            e.preventDefault();
-            if(username.value===''|| useremail===''){
-                msg.innerHTML='*please enter all fields'
-                //         // we can alos set timer to msg
-                        setTimeout(()=>msg.remove(),5000) 
-            }
-            else{
-                // create list item
-                var li = document.createElement('li')
-                
-                // create edit btn
-                let editbtn = document.createElement('button')
-                editbtn.className = 'btn  bg-primary btn-sm text-white edit rounded-5 ml-2 float-right';
-                // editbtn.value='edit'
-                 editbtn.appendChild(document.createTextNode('edit'));
-
-                // create delete btn
-                var deletebtn = document.createElement('button');
-                deletebtn.className = 'btn btn-danger btn-sm float-right my-4 delete ' ;
-                deletebtn.appendChild(document.createTextNode('X'))
-
-                // text for list
-                li.appendChild(document.createTextNode(`${username.value}-${useremail.value}-${userphone.value}`));
-                li.appendChild(editbtn)
-                li.appendChild(deletebtn);
-                // li.appendChild(document.createTextNode(useremail.value))
-                // li.appendChild(document.createTextNode(userphone.value))
-        
-                // adding value to localstorage in the form of object
-                let obj={
-                    name:username.value,
-                    email:useremail.value,
-                    phone:userphone.value,
-                }
-      
-                let str_obj=JSON.stringify(obj);
-                
-                
-                localStorage.setItem(useremail.value,str_obj)
-        
-                itemul.appendChild(li)
-        
-                // clear input
-                username.value='';
-                useremail.value='';
-                userphone.value='';
-        }
-        }
-
-        // removing function 
-        function removeitem(e){
-            if(e.target.classList.contains('delete')){
-                if (confirm('Do you want to remove item from LocalStorage?')) {
-                    var li = e.target.parentElement;
-
-                    // access li text & split email from it & delte from local storage
-                    var litext=li.textContent;
-                    let arr = litext.split('-');
-                    localStorage.removeItem(arr[1])
-
-
-                    itemul.removeChild(li)
-        
-                }
-
-            }
-        }
+var myform=document.getElementById('addform');
+var useramount=document.getElementById('expense')
+var userdiscription=document.getElementById('description')
+var usercategory=document.getElementById('category')
+var listitem=document.getElementById('itemlist')
 
 
 
+myform.addEventListener('submit',Submit)
+function Submit(e){
+    e.preventDefault();
 
-        // function for edit btn
-        function edititem(e){
-            if(e.target.classList.contains('edit')){
-                var li = e.target.parentElement;
+    // creating list item
+    var li=document.createElement('li')
+    
 
-                // access li text & split email from it & delte from local storage
-                
-                var siblingtext=e.target.previousSibling;
-                var maintext=siblingtext.textContent
-                let arr = maintext.split('-');
-                username.value=arr[0]
-                useremail.value=arr[1];
-                userphone.value=arr[2]
+    // create edit button
+    let editbtn=document.createElement('button')
+    editbtn.className = 'btn  bg-primary btn-sm text-white edit  ml-2 float-right';
+    editbtn.appendChild(document.createTextNode('Edit'))
+    
 
+    // create delete button
+    var delbtn = document.createElement('button');
+                delbtn.className = 'btn btn-danger btn-sm float-right my-4 delete ' ;
+                delbtn.appendChild(document.createTextNode('X'))
 
-                localStorage.removeItem(arr[1])
+    // create text for li
+    li.appendChild(document.createTextNode(`${useramount.value}-${userdiscription.value}-${usercategory.value}`))
 
+    let obj={
+       amount:useramount.value,
+       description:userdiscription.value,
+       category:usercategory.value
+    }
 
-                itemul.removeChild(li)
-               
-            }
-        }
+    let str_obj=JSON.stringify(obj);
+    localStorage.setItem(usercategory.value,str_obj)
+
+    li.appendChild(editbtn);
+    li.appendChild(delbtn)
+    listitem.appendChild(li)
+    
+
+}
+
